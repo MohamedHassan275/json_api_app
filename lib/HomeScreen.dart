5a86a45bc12d7c470f5712ca5164e5bbb1fc7730
MohamedHassan275 <mohamed209059@gmail.com>
2020-04-25 04:30:35 +0200

    Initial commit

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -11,14 +11,25 @@ class HomeScreenApi extends StatefulWidget{
     return HomeScreenApiListner();
   }
 
-  Future<String> getData() async {
+  Future<List> getData() async {
     String myUrl = 'https://jsonplaceholder.typicode.com/posts';
     http.Response response = await http.get(myUrl);
-    return response.body;
+    return json.decode(response.body);
   }
+
+
 }
 
 class HomeScreenApiListner extends State<HomeScreenApi>{
+
+  getMyData() async{
+    var homeApi =new HomeScreenApi();
+    List data;
+
+    data = await(homeApi.getData());
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -29,13 +40,15 @@ class HomeScreenApiListner extends State<HomeScreenApi>{
       body: new Container(
         alignment: Alignment.center,
         child: new Center(
-          child: new Text('Hello Mohamed',
-          maxLines: 1,
-          style: new TextStyle(fontSize: 22,
-          color: Colors.deepPurple,
-          fontStyle: FontStyle.italic,
-          ),
-          ),
+          child: new RaisedButton(onPressed: getMyData, child: new Text('Click me '),)
+
+//          new Text('Hello Mohamed',
+//          maxLines: 1,
+//          style: new TextStyle(fontSize: 22,
+//          color: Colors.deepPurple,
+//          fontStyle: FontStyle.italic,
+//          ),
+//          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:json_api_app/HomeScreen.dart';
 
 void main() async {
 
-  var homeScreenApi = new HomeScreenApi();
-  String data = await(homeScreenApi.getData());
-  print(data);
+//  var homeScreenApi = new HomeScreenApi();
+//  String data = await(homeScreenApi.getData());
+//
+//  for(int i =0 ; i < data.length ; i++){
+//    print('title =  ${data[i]}');
+//  }
   runApp(new MaterialApp(
     home: HomeScreenApi(),
     title: 'HomeScree',
